2c245e87fb737239c786e6781a5f5ae1aaefd63a
SUDHIKSHA-CODES <sudhiksha863@gmail.com>
2026-07-15 22:09:27 +0530

    Add predefined stock prices

--- codealpha.py
+++ codealpha.py
@@ -1,1 +1,13 @@
 print("Stock Portfolio Tracker")
+stock_prices = {
+    "AAPL": 180,
+    "TSLA": 250,
+    "GOOGL": 140,
+    "MSFT": 320,
+    "AMZN": 145
+}
+
+print("Available Stocks:")
+
+for stock, price in stock_prices.items():
+    print(stock, "-", price)
