4b48724829383089699c93370cbf10d7c2a2232a
SUDHIKSHA-CODES <sudhiksha863@gmail.com>
2026-07-15 22:10:54 +0530

    Add predefined stock prices

--- codealpha.py
+++ codealpha.py
@@ -11,3 +11,19 @@ print("Available Stocks:")
 
 for stock, price in stock_prices.items():
     print(stock, "-", price)
+    stock_prices = {
+    "AAPL": 180,
+    "TSLA": 250,
+    "GOOGL": 140,
+    "MSFT": 320,
+    "AMZN": 145
+}
+
+stock = input("Enter Stock Name: ").upper()
+quantity = int(input("Enter Quantity: "))
+
+if stock in stock_prices:
+    print("Stock Selected:", stock)
+    print("Quantity:", quantity)
+else:
+    print("Stock not available.")
